e5ddcf6c168cf859da228bf58e4680a29fb0868a
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-02-25 19:33:39 +0200

    updated js hw 7

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@ const inputField = document.querySelector("input#name-input");
 const outputField = document.querySelector("span#name-output");
 
 inputField.addEventListener("input", (handler) => {
-  outputField.textContent = handler.target.value;
-  if (!handler.target.value) {
+  outputField.textContent = handler.target.value.trim();
+  if (!outputField.textContent) {
     outputField.textContent = "Anonymous";
   }
 });
